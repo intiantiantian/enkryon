d5a623c6e740c72bfa8967cab4067cbb98f19d78
intian <villariacj@gmail.com>
2026-07-15 03:30:26 +0800

    fix(database): enforce SQLite foreign keys

--- database/connection.py
+++ database/connection.py
@@ -2,5 +2,8 @@ import sqlite3
 
 DATABASE_NAME = 'database/database.db'
 
-def connect_database():    
-    return sqlite3.connect(DATABASE_NAME)
+
+def connect_database():
+    connection = sqlite3.connect(DATABASE_NAME)
+    connection.execute("PRAGMA foreign_keys = ON")
+    return connection
